fb10b8f5e20cf187429a31326e02b44495e1a09c
Kashaf Ismail <kashafismail10@gmail.com>
2025-12-09 22:42:36 +0500

    update

---
+++ src/ReportsTabs/ExportTools.jsx
@@ -0,0 +1,3 @@
+export default function ExportTools() {
+  return <h2 style={{ padding: "20px" }}>Export Tools</h2>;
+}
---
+++ src/ReportsTabs/TransactionReport.jsx
@@ -0,0 +1,3 @@
+export default function TransactionReport() {
+  return <h2 style={{ padding: "20px" }}>Transaction Report</h2>;
+}
